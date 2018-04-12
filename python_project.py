c52d94fecaa38b886d3fa6e81a72155575d28f73
Louise <louise.mccann@outlook.com>
2018-04-12 14:55:55 +0100

    compare methods

--- python_project.py
+++ python_project.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 import matplotlib
 import scipy.special
 
+
 # ---------- numerical -------------------
-def tridiagonal_matrix(x, Ec=0.214, Ej=52, ng=0):
+def tridiagonal_matrix(x, Ec=0.214, Ej=52, ng=0.0):
     # where x is the size of the matrix
     # x should be even
     # allows for negative values of n
@@ -14,6 +15,7 @@ def tridiagonal_matrix(x, Ec=0.214, Ej=52, ng=0):
     vals, vectors = scipy.linalg.eigh_tridiagonal(diag, off_diag)
     vals = np.real(vals)
     return vals, vectors
+
 
 def plot_eigenvector(vector):
     # create a heatmap plot of eigenvectors
@@ -32,28 +34,38 @@ plot_eigenvector(simple_vectors)
 
 
 # -------------- analytical ----------------------------
-def analytical_energy(k, Ec=0.214, Ej=52, ng=0):
-    q = -2*Ej / Ec
-    r = k+1 - (k+1)%2 + 2*ng*(-1)**k
-    mat = 4*Ec*scipy.special.mathieu_a(r,q)
+def analytical_energy(k, Ec=0.214, Ej=52, ng=0.0):
+    q = -2 * Ej / Ec
+    r = k + 1 - (k + 1) % 2 + 2 * ng * (-1) ** k
+    mat = 4 * Ec * scipy.special.mathieu_a(int(r), q)
     return mat
 
 
-def analytical_wavefunction(k, Ec=0.214, Ej=52, ng=0):
+def analytical_wavefunction(k, Ec=0.214, Ej=52, ng=0.0):
     # generates a wavefunction between -pi and pi
     Ek = analytical_energy(k, Ec, Ej, ng)
-    const1 = int(4*Ek/Ec)
-    const2 = -2*Ej/Ec
-    print(Ek,const1,const2)
-    pi_range = np.arange(-np.pi, np.pi,0.01)
-    mathieu_c = np.array([scipy.special.mathieu_cem(const1, const2, theta/2.0)[0] for theta in pi_range])
-    mathieu_s = np.array([scipy.special.mathieu_sem(const1, const2, theta/2.0)[0] for theta in pi_range])
-    ex = np.exp([(1j *ng *theta)/np.sqrt(2*np.pi) for theta in pi_range])
-    return ex*(mathieu_c + (1j*(-1)**(k+1))*mathieu_s)
-    #return ex, mathieu_c, mathieu_s
+    const1 = int(4 * Ek / Ec)
+    const2 = -2 * Ej / Ec
+    print(Ek, const1, const2)
+    pi_range = np.arange(-np.pi, np.pi, 0.01)
+    mathieu_c = np.array([scipy.special.mathieu_cem(const1, const2, theta / 2.0)[0] for theta in pi_range])
+    mathieu_s = np.array([scipy.special.mathieu_sem(const1, const2, theta / 2.0)[0] for theta in pi_range])
+    ex = np.exp([(1j * ng * theta) / np.sqrt(2 * np.pi) for theta in pi_range])
+    return ex * (mathieu_c + (1j * (-1) ** (k + 1)) * mathieu_s)
+    # return ex, mathieu_c, mathieu_s
 
 
-a = analytical_wavefunction(1,1,1)
-#print(a)
-plt.plot(np.arange(-np.pi, np.pi,0.01), abs(a)**2)
+a = analytical_wavefunction(1, 1, 1)
+# print(a)
+plt.plot(np.arange(-np.pi, np.pi, 0.01), abs(a) ** 2)
+plt.show()
+
+# ------------------------- compare methods ---------------------
+ej = 52.0
+simple_vals, simple_vectors = tridiagonal_matrix(s, ng=0.1)
+
+plt.plot(np.real(simple_vals) / ej)
+plt.show()
+ana = [analytical_energy(i, ng=0.1) / ej for i in range(0, s)]
+plt.plot(ana)
 plt.show()
