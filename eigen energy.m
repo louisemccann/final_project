86b39f0c708423ee14869da5b022f42ab07e004b
Louise <louise.mccann@outlook.com>
2018-04-23 12:36:10 +0100

    code cleanup

--- eigen energy.m
+++ eigen energy.m
@@ -24,5 +24,5 @@ EC0 = 0.214; EJ0 = 52; ng0 = 0;
 Emkoch[m0_] := Module[{m=m0},EC0; EJ = EJ0; NG = ng0;
 		k = (Round[Mod[2*NG + -1/2.0,2]] * Round[NG + (-1*(-1)^m) *Quotient[m+1,2]]) + (Round[Mod[2*NG + 1/2.0,2]] * Round[NG + (1*(-1)^m) *Quotient[m+1,2]]);
     EC0*MathieuCharacteristicA[2,NG+k]*(-EJ0/(2*EC0))];
-data2 := Table[Emkoch[x],{x,0,40}]
+data2 := Table[Emkoch[x],{x,0,40}];
 Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data2.dat",data2 ];
--- python_project.py
+++ python_project.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import matplotlib
 import scipy.special
 import math
+
 matplotlib.rcParams['figure.figsize'] = (15, 10)
+
 
 # ---------- numerical -------------------
 def tridiagonal_matrix(x, Ec=0.214, Ej=52, ng=0.0):
@@ -21,7 +23,7 @@ def tridiagonal_matrix(x, Ec=0.214, Ej=52, ng=0.0):
 def plot_eigenvector(vector, m):
     # create a heatmap plot of eigenvectors
     matplotlib.rcParams['figure.figsize'] = (15, 15)
-    plt.imshow(abs(vector),extent=[0,m/2,-m/4,m/4])
+    plt.imshow(abs(vector), extent=[0, m / 2, -m / 4, m / 4])
     plt.colorbar(orientation='vertical')
     plt.xlabel("Matrix Element, m")
     plt.ylabel("Charge State, n")
@@ -29,7 +31,8 @@ def plot_eigenvector(vector, m):
     plt.savefig("figs/matrix_plot_of_eigenvectors.png")
     plt.show()
 
-def plot_eigenvalues(values,ej=52):
+
+def plot_eigenvalues(values, ej=52):
     matplotlib.rcParams['figure.figsize'] = (15, 10)
     plt.plot(np.real(values)[:40] / ej)
     plt.xlabel("Matrix Element, m")
@@ -37,11 +40,11 @@ def plot_eigenvalues(values,ej=52):
     plt.savefig("figs/plot_of_eigenvalues.png")
     plt.show()
 
+
 s = 80
 simple_vals, simple_vectors = tridiagonal_matrix(s)
 plot_eigenvector(simple_vectors, m=s)
 plot_eigenvalues(simple_vals)
-
 
 
 # -------------- analytical ----------------------------
@@ -66,6 +69,8 @@ def analytical_wavefunction(k, Ec=0.214, Ej=52, ng=0.0):
 
 
 a = analytical_wavefunction(1, 1, 1)
+
+
 # print(a)
 # plt.plot(np.arange(-np.pi, np.pi, 0.01), abs(a) ** 2)
 # plt.show()
@@ -73,10 +78,10 @@ a = analytical_wavefunction(1, 1, 1)
 # ------------------------- compare methods ---------------------
 
 def plot_analytical_energy(p_values, m_values, ej=52):
-    #plots mathematica and python analytical values
+    # plots mathematica and python analytical values
     matplotlib.rcParams['figure.figsize'] = (15, 10)
     plt.plot(np.real(p_values) / ej, label='Python')
-    plt.plot(m_values/ ej, label='Mathematica')
+    plt.plot(m_values / ej, label='Mathematica')
     plt.xlabel("Matrix Element, m")
     plt.ylabel("Em/Ej")
     plt.legend()
@@ -84,34 +89,35 @@ def plot_analytical_energy(p_values, m_values, ej=52):
 
 ana = np.array([analytical_energy(i, ng=1E-10) for i in np.arange(0, s)])
 mathematica = np.loadtxt("data_ng_nearly0.dat", dtype=float)
-plot_analytical_energy(ana,mathematica)
+plot_analytical_energy(ana, mathematica)
 plt.savefig("figs/compare_python_mathematica.png")
 plt.show()
-#^^ the python part of this graph is really weird?
+# ^^ the python part of this graph is really weird?
 # it's kind of backwards
 # maybe don't use this
 # compare the python and mathematica functions using ng=0 exactly, comment that this is a special case
 
 ana0 = np.array([analytical_energy(i, ng=0.0) for i in np.arange(0, s)])
 mathematica0 = np.loadtxt("data_ng_0.dat", dtype=float)
-plot_analytical_energy(ana0,mathematica0)
+plot_analytical_energy(ana0, mathematica0)
 plt.savefig("figs/compare_python_mathematica_ng0.png")
 plt.show()
 
 # compare mathematica and simple values
-ej=52
+ej = 52
 matplotlib.rcParams['figure.figsize'] = (15, 8)
-fig,ax=plt.subplots(1,2)
+fig, ax = plt.subplots(1, 2)
 ax[0].plot(simple_vals[:40], label='Numerical')
 plt.xlabel("Matrix Element, m")
 plt.ylabel("Em/Ej")
 plt.legend()
-plt.plot(mathematica[:40]/16.0, label='Analytical', color='orange')
+plt.plot(mathematica[:40] / 16.0, label='Analytical', color='orange')
 plt.xlabel("Matrix Element, m")
 plt.ylabel("Em/Ej")
 plt.legend()
 plt.savefig("figs/compare_analytical_numerical.png")
 plt.show()
+
 
 # ---------------- koch method ----------------
 
@@ -122,40 +128,44 @@ def k_function(m, ng=0.0):
     k += round((2 * ng + l / 2.0) % 2) * (int(ng + (l * (-1) ** m) * ((m + 1) // 2.0)))
     return k
 
-def em_koch(m, ng=0.0,Ec=0.214, Ej=52):
-    return Ec * scipy.special.mathieu_a(2, ng + np.real(k_function(m,ng))) * (-Ej/(2*Ec))
+
+def em_koch(m, ng=0.0, Ec=0.214, Ej=52):
+    return Ec * scipy.special.mathieu_a(2, ng + np.real(k_function(m, ng))) * (-Ej / (2 * Ec))
 
 
-k_test = [k_function(m) for m in range(1,40)]
-koch_test = [em_koch(m,ng=1E-10) for m in range(1,40)]
-plt.plot(range(1,40),np.array(koch_test)/52.0, label='koch')
+k_test = [k_function(m) for m in range(1, 40)]
+koch_test = [em_koch(m, ng=0.0) for m in range(1, 40)]
+plt.plot(range(1, 40), np.array(koch_test) / 52.0, label='koch')
 plt.show()
 
-#t = np.loadtxt("data2.dat")
-#plt.plot(t)
-#plt.show()
 
-def koch_wavefunction(m, ng=0.000001,Ec=0.214, Ej=52):
+# t = np.loadtxt("data2.dat")
+# plt.plot(t)
+# plt.show()
+
+def koch_wavefunction(m, ng=0.000001, Ec=0.214, Ej=52):
     # finds the wavefunction according to koch paper
     # for phi = -2pi to 2pi
-    #for phi in :
-        #psi = np.exp(1j *ng * phi)/np.sqrt(2.0)
-        #mat = scipy.special.mathieu_cem(-2*(ng-k_function(m,ng)),-Ej/2*Ec,phi/2.0)[0]
-    r = -2*(ng-k_function(m,ng))
-    print(m,k_function(m,ng),int(r))
-    q = -Ej/2*Ec
-    return np.array([(np.exp(1j *ng * phi)/np.sqrt(2.0))*scipy.special.mathieu_cem(int(r),q,phi/2.0)[0] for phi in np.arange(-2*np.pi, 2*np.pi, 0.1)])
+    # for phi in :
+    # psi = np.exp(1j *ng * phi)/np.sqrt(2.0)
+    # mat = scipy.special.mathieu_cem(-2*(ng-k_function(m,ng)),-Ej/2*Ec,phi/2.0)[0]
+    r = -2 * (ng - k_function(m, ng))
+    print(m, k_function(m, ng), int(r))
+    q = -Ej / 2 * Ec
+    return np.array(
+        [(np.exp(1j * ng * phi) / np.sqrt(2.0)) * scipy.special.mathieu_cem(int(r), q, phi / 2.0)[0] for phi in
+         np.arange(-2 * np.pi, 2 * np.pi, 0.1)])
 
 
-for m in [1,11,21,41,51,81]:
+for m in [1, 11, 21, 41, 51, 81]:
     wave_test = koch_wavefunction(m)
-    plt.plot(wave_test, label='%s' %m)
+    plt.plot(wave_test, label='%s' % m)
 plt.legend()
 plt.show()
 
-for m in [1,11,21,41,51,81]:
+for m in [1, 11, 21, 41, 51, 81]:
     wave_test = koch_wavefunction(m)
     prob = np.absolute(wave_test ** 2)
-    plt.plot(prob, label='%s' %m)
+    plt.plot(prob, label='%s' % m)
 plt.legend()
 plt.show()
