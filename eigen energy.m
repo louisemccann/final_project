abb4f1a231b47195fd406e37deef653ca83dd7f5
Louise <louise.mccann@outlook.com>
2018-05-17 14:09:43 +0100

    results redo

--- eigen energy.m
+++ eigen energy.m
@@ -24,22 +24,38 @@ EC0 = 0.214; EJ0 = 52;
 Emkoch[m0_, ng0_] := Module[{m=m0, ng=ng0},EC0; EJ = EJ0;
 		k = (Round[Mod[2*ng + (-1/2.0),2]] * (Round[ng] + -1*(-1)^m) *Quotient[m+1,2]) + (Round[Mod[2*ng + (1/2.0),2]] * (Round[ng] + 1*(-1)^m) *Quotient[m+1,2]);
     EC0*MathieuCharacteristicA[(2*(ng+k)),(-EJ0/(2*EC0))]];
+    
+data2 := Table[Emkoch[x,0.0],{x,0,40}];
+Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data2.dat",data2 ];
+    
 data2 := Table[Emkoch[x,0],{x,0,40}];
 Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data\\data_ng_0.dat",data2 ];
-data2 := Table[Emkoch[x,0.01],{x,0,40}];
+data2 := Table[Emkoch[x,0.1],{x,0,40}];
+Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data\\data_ng_01.dat",data2 ];
+data2 := Table[Emkoch[x,0.2],{x,0,40}];
 Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data\\data_ng_02.dat",data2 ];
+data2 := Table[Emkoch[x,0.3],{x,0,40}];
+Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data\\data_ng_03.dat",data2 ];
 data2 := Table[Emkoch[x,0.4],{x,0,40}];
 Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data\\data_ng_04.dat",data2 ];
-data2 := Table[Emkoch[x,0.6],{x,0,40}];
-Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data\\data_ng_06.dat",data2 ];
-data2 := Table[Emkoch[x,0.8],{x,0,40}];
-Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data\\data_ng_08.dat",data2 ];
+data2 := Table[Emkoch[x,0.5],{x,0,40}];
+{
+ {Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data\\data_ng_05.dat",data2 ];},
+ {data2 := Table[Emkoch[x,0.6],{x,0,40}];
+  Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data\\data_ng_06.dat",data2 ];},
+ {\[Placeholder]}
+}
 
 wavekoch[m0_] := Module[{m=m0},EC = EC0; EJ = EJ0; NG = ng0;
 				r = -2 * (NG- Emkoch[m0])
 				q = -EJ/2*EC
 				Table[Module[{},(Exp[I*NG*phi] / Sqrt[2]) * MathieuC[r,q,phi/2]], {phi, -2*Pi, 2*Pi}]];
 
-
+EC0 = 0.214; EJ0 = 52; ng0 = 1E-5;
+Emkoch[m0_] := Module[{m=m0},EC0; EJ = EJ0; NG = ng0;
+		k = (Round[Mod[2*NG + -1/2.0,2]] * (Round[NG] + -1*(-1)^m) *Quotient[m+1,2]) + (Round[Mod[2*NG + 1/2.0,2]] * (Round[NG] + 1*(-1)^m) *Quotient[m+1,2]);
+    EC0*MathieuCharacteristicA[(2*NG+k),(-EJ0/(2*EC0))]];
+data2 := Table[Emkoch[x],{x,0,40}];
+Export["C:\\Users\\Louise\\Documents\\project\\final_project\\data2.dat",data2 ];
 
 
--- individual graph.py
+++ individual graph.py
@@ -6,41 +6,13 @@ import scipy.special
 import scipy.sparse
 import math
 
-matplotlib.rcParams['figure.figsize'] = (15, 10)
+matplotlib.rcParams['figure.figsize'] = (15, 15)
 matplotlib.rcParams.update({'font.size': 22})
 
+root = 'data/data_ng_0'
+add = ['',1,2,3,4,5]
+for i in add:
+    plt.plot(np.loadtxt(root+str(i)+'.dat')/52, label=str(i))
+plt.legend()
+plt.show()
 
-def tridiagonal_matrix(x, Ec=0.214, Ej=52, ng=0.0):
-    # where x is the size of the matrix
-    # x should be even
-    # allows for negative values of n
-    diag = [4 * Ec * (m - ng) ** 2 for m in range(int(-x / 2), int(x / 2))]
-    off_diag = [-Ej / 2] * (x - 1)
-    vals, vectors = scipy.linalg.eigh_tridiagonal(diag, off_diag)
-    vals = np.real(vals)
-    return vals, vectors
-
-
-def plot_eigenvector(vector, m):
-    # create a heatmap plot of eigenvectors
-    # matplotlib.rcParams['figure.figsize'] = (15, 15)
-    plt.imshow(abs(vector), extent=[0, m / 2, -m / 2, m / 2], aspect='auto')
-    plt.colorbar(orientation='vertical')
-    plt.xlabel("Matrix Element, m")
-    plt.ylabel("Charge State, n")
-    plt.savefig("figs/matrixplotofeigenvectors.png", Transparent=True)
-    plt.show()
-
-
-def plot_eigenvalues(values, ej=52):
-    # matplotlib.rcParams['figure.figsize'] = (15, 10)
-    plt.plot(np.real(values)[:40] / ej)
-    plt.xlabel("Matrix Element, m")
-    plt.ylabel("$E_m / E_J$")
-    plt.savefig("figs/plot_of_eigenvalues.png")
-    plt.show()
-
-
-s = 80
-simple_vals, simple_vectors = tridiagonal_matrix(s)
-plot_eigenvector(simple_vectors, m=s)
